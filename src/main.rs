1f5383d99cc8705a08251b9dc982ee0822c86d81
Artem <rtkushner@gmail.com>
2022-09-21 18:32:15 +0200

    imporper ordering in multibyte case

--- src/main.rs
+++ src/main.rs
@@ -74,13 +74,14 @@ pub fn pack_pre_post_balances(meta: &Value) -> Vec<u8> {
     );
 
     let n_account_octets: usize = pre_array.len() / 8 + 1;
-    let encoded = vec![0u8; n_account_octets + 1];
 
     let mut change_accumulator: u128 = 0;
-    let mut pre_balances: Vec<u64> = pre_array.iter().map(|v| v.as_u64().unwrap()).collect();
+    let pre_balances: Vec<u64> = pre_array.iter().map(|v| v.as_u64().unwrap()).collect();
     let mut post_balances: Vec<u64> = vec![];
     // println!("Got accumulator :{}", accumulator);
+
     for (i, (pre, post)) in pre_array.iter().zip(post_array.iter()).enumerate() {
+
         if pre != post {
             change_accumulator += 2_u128.pow(i as u32);
             post_balances.push(post.as_u64().unwrap());
@@ -92,10 +93,6 @@ pub fn pack_pre_post_balances(meta: &Value) -> Vec<u8> {
         .into_iter()
         .take(n_account_octets)
         .collect();
-
-    println!("Len of pre {}", pre_array.len());
-    println!("Number of octets : {:?}", n_account_octets);
-    println!("CAST : {:?}", vecu8_to_binary_string(&change_bitfield));
 
     [post_balances, pre_balances]
         .concat()
@@ -114,6 +111,8 @@ pub fn vecu8_to_binary_string(vecu8: &[u8]) -> Vec<String> {
 
 #[cfg(test)]
 mod tests {
+    use std::io::Read;
+
     use serde_json::Value;
 
     use crate::pack_pre_post_balances;
@@ -132,6 +131,7 @@ mod tests {
             let _                    = balance_vals.iter().for_each(|v| head.extend_from_slice(&v.to_le_bytes()));
             assert_eq!(pack_pre_post_balances(balances),head);
     }
+
     #[test]
     fn pre_post_allchange() {
         let balances = b"
@@ -165,6 +165,7 @@ mod tests {
             let _                    = balance_vals.iter().for_each(|v| head.extend_from_slice(&v.to_le_bytes()));
             assert_eq!(pack_pre_post_balances(balances),head);
     }
+
     #[test]
     fn pre_post_multioctet_nochange() { 
         let balances = b"
@@ -181,6 +182,30 @@ mod tests {
             let mut head             = vec![0_u8, 0_u8, 0_u8];
             let balance_vals:[u64;17] = [
                     10000000004,20000000003,300000000,4000000,5,60000000004,70000000003,800000000,9000000,10,11000000004,12000000003,130000000,1400000,15,1600000,17,
+            ];
+            let _                    = balance_vals.iter().for_each(|v| head.extend_from_slice(&v.to_le_bytes()));
+            assert_eq!(pack_pre_post_balances(balances),head);
+    }
+
+    #[test]
+    fn pre_post_multioctet_changed() { 
+        let balances = b"
+            {
+            \"preBalances\": [
+
+                    10000000004,20000000003,300000000,4000000,5,60000000004,70000000003,800000000,9000000,10,11000000004,12000000003,130000000,1400000,15,1600000,17
+            ],
+            \"postBalances\" : [
+                    10000000004,20000000003,300000000,4000000,5555,60000000004,70000000003,800000000,9000000,10222,11000000004,12000000003,130000000,1400000,15,1600000,17000
+            ]  
+            }";
+            let balances: &Value = &serde_json::from_slice(balances).unwrap();
+            // println!("Chnaged :{:#0130b}",( (2_i32.pow(4) + 2_i32.pow(9)  + 2_i32.pow(16)) as u128 ));
+            // println!("into bytes :{:#?}",( (2_i32.pow(4) + 2_i32.pow(9)  + 2_i32.pow(16)) as u128 ).to_le_bytes());
+            // println!("into bytes :{:#?}",( (2_i32.pow(4) + 2_i32.pow(9)  + 2_i32.pow(16)) as u128 ).to_le_bytes());
+            let mut head:Vec<u8> = ( (2_i32.pow(4) + 2_i32.pow(9)  + 2_i32.pow(16)) as u128 ).to_le_bytes()[0..3].to_vec();
+            let balance_vals:[u64;20] = [
+                    10000000004,20000000003,300000000,4000000,5,60000000004,70000000003,800000000,9000000,10,11000000004,12000000003,130000000,1400000,15,1600000,17,5555,10222,17000
             ];
             let _                    = balance_vals.iter().for_each(|v| head.extend_from_slice(&v.to_le_bytes()));
             assert_eq!(pack_pre_post_balances(balances),head);
