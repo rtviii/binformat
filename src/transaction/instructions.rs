49a66c383043374417e7d08f83ff6adeed820bfe
Artem <rtkushner@gmail.com>
2022-09-24 11:16:24 -0500

    settign ix tests

--- src/transaction/instructions.rs
+++ src/transaction/instructions.rs
@@ -1,5 +1,8 @@
+use std::vec;
+
 use serde_json::Value;
 pub fn pack_ix(ix: &Value) -> Vec<u8> {
+
     let ix_prog_index = &ix["programIdIndex"]
         .as_u64()
         .unwrap_or_else(|| panic!("Failed to unpack programIdIndex: {:?}", ix));
@@ -16,22 +19,19 @@ pub fn pack_ix(ix: &Value) -> Vec<u8> {
         .into_iter()
         .map(|a| a.as_u64().unwrap() as u8)
         .collect();
+
     let accounts_len = ix_accounts.len() as u8;
-    let data_bytes = bs58::decode(ix_data).into_vec().unwrap();
-    let data_len = (data_bytes.len() as u16).to_le_bytes();
-
-    println!("accounts_len :{}", accounts_len);
-    println!("accounts  :{:?}", ix_accounts);
-    println!("data  :{:?}", ix_data);
-    println!("data len  :{:?}", data_len);
-
-    let result = vec![*ix_prog_index as u8, accounts_len, data_len[0]];
-    let yx = [[*ix_prog_index as u8, accounts_len], data_len].concat();
+    let data_bytes   = bs58::decode(ix_data).into_vec().unwrap();
+    let data_len     = (data_bytes.len() as u16).to_le_bytes();
+    let result       = vec![*ix_prog_index as u8, accounts_len, data_len[0]];
+    let yx           = [[*ix_prog_index as u8, accounts_len], data_len].concat();
 
     [yx, acc_indices_u8, data_bytes].concat()
 }
 
-pub fn unpack_ix() {}
+pub fn unpack_ix(buffer:&[u8])->(u8,Vec<u8>,Vec<u8>){
+    (0,vec![],vec![])
+}
 
 pub fn pack_inner_ix() {}
 
@@ -39,7 +39,9 @@ pub fn unpack_inner_ix() {}
 
 #[cfg(test)]
 mod tests {
-    use crate::transaction::instructions::pack_ix;
+    use std::{fs::File, io::{Write, BufReader, Read}};
+
+    use crate::transaction::{instructions::{pack_ix, unpack_ix}, balances::unpack_pre_post_balances};
 
     #[test]
     fn test_pack_ix() {
@@ -55,13 +57,12 @@ mod tests {
                                 "programIdIndex": 21
                             }
         "#;
-        println!("serialized meta: {}", sample_ix);
         let packed = pack_ix(&serde_json::from_str(sample_ix).unwrap());
 
         let s = vec![
             0x15, //<-- prog index
             0x03, // <-- acc len
-            0x00, 0x08, // <-- data len,
+            0x09, 0x00, // <-- data len,
             0x03, 0x04, 0x00, // <-- acc indices
             0x03, 0x00, 0x27, 0xab, 0xce, 0x01, 0x00, 0x00, 0x00,
         ];
@@ -70,6 +71,34 @@ mod tests {
 
         // let shortdata = "3DV4nz1KFpQX";
         // let shortdata_hex = [0x03, 0x00, 0x27, 0xab, 0xce, 0x01, 0x00, 0x00, 0x00];
+    }
+
+    #[test]
+    fn test_pack_ix_longer() {
+
+        let sample_ix2 = r#"
+
+
+                            {
+                            "accounts": [
+                                    1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+                                ],
+                                "data": "29z5mr1JoRmJYQ6zJg9CHGgmenA3L6MvJTPz7rD2zwhmLMNsv78oAGGcxPCLGYhWT673uUjfqnEjHmzUbJGxfF1bKgVo9h",
+                                "programIdIndex": 42
+                            }
+        "#;
+
+        let s2 = vec![
+            0x2A, //<-- prog index
+            0x10, // <-- acc len
+            0x45, 0x00, // <-- data len(69 = 64+5) 
+            0x01, 0x02, 0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10, // <-- acc indices
+            0x02, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xfa, 0x2c,
+            0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0xfb, 0x2c, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00,
+            0xe3, 0x53, 0x4b, 0x39, 0xb0, 0x34, 0x01, 0xce, 0x1b, 0xd5, 0x67, 0x95, 0x75, 0xa4,
+            0xb5, 0xa4, 0x75, 0x9a, 0x77, 0x36, 0x71, 0xbe, 0x63, 0xd7, 0xa0, 0x89, 0xae, 0xaf,
+            0x95, 0xb4, 0x60, 0x6c, 0x01, 0xf6, 0x72, 0x84, 0x62, 0x00, 0x00, 0x00, 0x00,
+        ];
         // let ixdata__    = "29z5mr1JoRmJYQ6zJg9CHGgmenA3L6MvJTPz7rD2zwhmLMNsv78oAGGcxPCLGYhWT673uUjfqnEjHmzUbJGxfF1bKgVo9h".to_string();
         // let ixdata__hex = [
         //     0x02, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xfa, 0x2c,
@@ -78,11 +107,61 @@ mod tests {
         //     0xb5, 0xa4, 0x75, 0x9a, 0x77, 0x36, 0x71, 0xbe, 0x63, 0xd7, 0xa0, 0x89, 0xae, 0xaf,
         //     0x95, 0xb4, 0x60, 0x6c, 0x01, 0xf6, 0x72, 0x84, 0x62, 0x00, 0x00, 0x00, 0x00,
         // ];
-
         // println!("hash : {}", ixdata__);
         // println!("hash as slice: {:04X?}", ixdata__.as_bytes());
+
+        let packed = pack_ix(&serde_json::from_str(sample_ix2).unwrap());
+        assert_eq!(packed, s2);
+
     }
 
     #[test]
-    fn test_pack_ix_1232_data() {}
+    fn test_pack_ix_1232_data() {
+        let path = "ixtest1.beach";
+        let sample_ix2 = r#"
+
+
+                            {
+                            "accounts": [
+                                    1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+                                ],
+                                "data": "29z5mr1JoRmJYQ6zJg9CHGgmenA3L6MvJTPz7rD2zwhmLMNsv78oAGGcxPCLGYhWT673uUjfqnEjHmzUbJGxfF1bKgVo9h",
+                                "programIdIndex": 42
+                            }
+        "#;
+
+        let s2 = vec![
+            0x2A, //<-- prog index
+            0x10, // <-- acc len
+            0x45, 0x00, // <-- data len(69 = 64+5) 
+            0x01, 0x02, 0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10, // <-- acc indices
+            0x02, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xfa, 0x2c,
+            0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0xfb, 0x2c, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00,
+            0xe3, 0x53, 0x4b, 0x39, 0xb0, 0x34, 0x01, 0xce, 0x1b, 0xd5, 0x67, 0x95, 0x75, 0xa4,
+            0xb5, 0xa4, 0x75, 0x9a, 0x77, 0x36, 0x71, 0xbe, 0x63, 0xd7, 0xa0, 0x89, 0xae, 0xaf,
+            0x95, 0xb4, 0x60, 0x6c, 0x01, 0xf6, 0x72, 0x84, 0x62, 0x00, 0x00, 0x00, 0x00,
+        ];
+
+        // ...
+        
+        let packed = pack_ix(&serde_json::from_str(sample_ix2).unwrap());
+        let mut f  = File::create(path).unwrap();
+        f.write_all(packed.as_slice()).unwrap();
+
+        let f                   = File::open(path).unwrap();
+        let mut reader          = BufReader::new(f);
+        let mut buffer: Vec<u8> = Vec::new();
+        reader.read_to_end(&mut buffer).unwrap();
+
+        let (prog_index,accounts,data) = unpack_ix(&buffer);
+
+        assert_eq!(prog_index,   42);
+        assert_eq!(accounts,    [0x01, 0x02, 0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10]);
+        assert_eq!(data,        [0x02, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xfa, 0x2c,
+                                 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0xfb, 0x2c, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00,
+                                 0xe3, 0x53, 0x4b, 0x39, 0xb0, 0x34, 0x01, 0xce, 0x1b, 0xd5, 0x67, 0x95, 0x75, 0xa4,
+                                 0xb5, 0xa4, 0x75, 0x9a, 0x77, 0x36, 0x71, 0xbe, 0x63, 0xd7, 0xa0, 0x89, 0xae, 0xaf,
+                                 0x95, 0xb4, 0x60, 0x6c, 0x01, 0xf6, 0x72, 0x84, 0x62, 0x00, 0x00, 0x00, 0x00]);
+        std::fs::remove_file(path).unwrap();
+    }
 }
