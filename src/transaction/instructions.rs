026385e4f88bcf3fb8b10ef44d4dca3e72c6785d
Artem <rtkushner@gmail.com>
2022-09-27 13:38:58 -0500

    cargo test -- --nocapture

--- src/transaction/instructions.rs
+++ src/transaction/instructions.rs
@@ -1,7 +1,11 @@
 use serde_json::Value;
 use solana_sdk::instruction::CompiledInstruction;
 use solana_transaction_status::InnerInstructions;
-use std::{vec, sync::{Arc, Mutex}, slice::SliceIndex};
+use std::{
+    slice::SliceIndex,
+    sync::{Arc, Mutex},
+    vec,
+};
 pub fn pack_ix(ix: &Value) -> Vec<u8> {
     let ix_prog_index = &ix["programIdIndex"]
         .as_u64()
@@ -29,22 +33,25 @@ pub fn pack_ix(ix: &Value) -> Vec<u8> {
     [yx, acc_indices_u8, data_bytes].concat()
 }
 
-pub fn unpack_ix(buffer: &[u8]) -> ( CompiledInstruction, usize  ){
-    let prog_index: u8       = buffer[0];
-    let acc_len              = buffer[1];
-    let data_len             = u16::from_le_bytes([buffer[2],buffer[3]]);
+pub fn unpack_ix(buffer: &[u8]) -> (CompiledInstruction, usize) {
+    let prog_index: u8 = buffer[0];
+    let acc_len = buffer[1];
+    let data_len = u16::from_le_bytes([buffer[2], buffer[3]]);
     let acc_indices: Vec<u8> = (4..4 + acc_len).map(|i| buffer[i as usize]).collect();
-    let data: Vec<u8>        = Vec::from(&buffer[(4 + acc_len as usize)..(( 4 + acc_len as u16 + data_len ) as usize)]);
-    ( CompiledInstruction{
-        accounts: acc_indices,
-        data,
-        program_id_index: prog_index,
-    }, ( 4 + acc_len as u16 + data_len ) as usize )
+    let data: Vec<u8> =
+        Vec::from(&buffer[(4 + acc_len as usize)..((4 + acc_len as u16 + data_len) as usize)]);
+    (
+        CompiledInstruction {
+            accounts: acc_indices,
+            data,
+            program_id_index: prog_index,
+        },
+        (4 + acc_len as u16 + data_len) as usize,
+    )
 }
 
-
-
-
+///`total_inner_ix_len` signifies how many bytes long is the size of this inner instruction: `CompiledInstruction`s (sum of individual lengths of each instructions) + 1 for `inner_ix_index`  + 2 bytes for itself is is enough information to be able to skip to the end of the given inner_instruction:
+///`jmp(total_inner_ix_len) is where the next inner instruction begins in the higher-level trnasaction structure.
 pub fn pack_inner_ix(v: &Value) -> Vec<u8> {
     let ix_inner_index = v["index"]
         .as_u64()
@@ -54,56 +61,67 @@ pub fn pack_inner_ix(v: &Value) -> Vec<u8> {
         .as_array()
         .unwrap_or_else(|| panic!("failed to unpack accounts {:?}", &v));
     let mut index_and_data = vec![ix_inner_index as u8];
-    for ix_inner_ix in ix_inner_instructions{
+    for ix_inner_ix in ix_inner_instructions {
         index_and_data.extend_from_slice(&pack_ix(ix_inner_ix))
     }
 
-    ///`total_inner_ix_len` signifies how many bytes long is the size of this inner instruction: `CompiledInstruction`s (sum of individual lengths of each instructions) + 1 for `inner_ix_index`  + 2 bytes for itself is is enough information to be able to skip to the end of the given inner_instruction:
-    ///`jmp(total_inner_ix_len) is where the next inner instruction begins in the higher-level trnasaction structure.
-    let mut r = (index_and_data.len() as u16 + 2 ).to_le_bytes().to_vec();
+    let mut r = (index_and_data.len() as u16 + 2).to_le_bytes().to_vec();
     r.append(&mut index_and_data);
     r
 }
 
-pub trait BeachOps{
-    fn extract_instruction(&mut self) -> CompiledInstruction;
+pub trait BeachOps {
+    fn extract_instruction(&self) -> CompiledInstruction;
 }
 
 /// This is to stand in for a general buffer with SBBF operation handles defined on it.
 /// Once a component has been extracted from the buffer, advance the offset.
 /// This is a general harness for buffers and should be differentiated further.
-pub struct BufferWindow<'life>{
+pub struct BufferWindow<'life> {
     pub buffer: &'life [u8],
     // how many bytes have been read so far.
-    offset: Arc<Mutex<usize>>
+    offset: Arc<Mutex<usize>>,
 }
 
-
-impl BeachOps for BufferWindow<'_>{
-    fn extract_instruction(&mut self) -> CompiledInstruction {
-        let              (ix, readlen)  = unpack_ix(&self.buffer[*self.offset.lock().expect("Failed to acquire lock")..]);
-        *self.offset.lock().expect("Could not acquire lock on the Beach buffer.")    += readlen;
+impl BeachOps for BufferWindow<'_> {
+    fn extract_instruction(&self) -> CompiledInstruction {
+        let (ix, readlen) =
+            unpack_ix(&self.buffer[*self.offset.lock().expect("Failed to acquire lock")..]);
+        *self
+            .offset
+            .lock()
+            .expect("Could not acquire lock on the Beach buffer.") += readlen;
         ix
     }
 }
 
-
-
 pub fn unpack_inner_ix(buffer: &[u8]) -> InnerInstructions {
 
-    let total_length = u16::from_le_bytes([buffer[1],buffer[2]]);
-    let index        = buffer[3];
-    let instructions = &buffer[4..];
+    let total_length          = u16::from_le_bytes([buffer[0], buffer[1]]);
+    println!("Got total length :{}", total_length);
+    let index                 = buffer[2];
+    let instruction_raw_bytes = &buffer[3..];
 
-    let bw = BufferWindow{
-        buffer: instructions,
-        offset: Arc::new(Mutex::new(0))
+    let bw = BufferWindow {
+        buffer: instruction_raw_bytes,
+        offset: Arc::new(Mutex::new(0)),
     };
 
+    let mut inner_ix_ixs = vec![];
+    if let Ok(offset) = bw.offset.try_lock() {
+        // while the offset of the buffer is still below the sum lengths of compiled instructions 
+        // there are instructions left in this inner_ix
+        while  *offset < ( total_length.checked_sub(2).expect("Total length less than 2. Soemthing went terribly wrong") ) as usize{
+            let ix = bw.extract_instruction();
+            inner_ix_ixs.push(ix);
+        }
+    } else {
+        panic!("Failed to acquire buffer lock.")
+    };
 
     InnerInstructions {
-        index: 2,
-        instructions: vec![],
+        index,
+        instructions: inner_ix_ixs
     }
 }
 
@@ -111,14 +129,15 @@ pub fn unpack_inner_ix(buffer: &[u8]) -> InnerInstructions {
 mod tests {
     use std::{
         fs::File,
-        io::{BufReader, Read, Write}, sync::{Arc, Mutex},
+        io::{BufReader, Read, Write},
+        sync::{Arc, Mutex},
     };
 
     use solana_sdk::instruction::CompiledInstruction;
     use solana_transaction_status::InnerInstructions;
 
-    use crate::transaction::{
-        instructions::{pack_inner_ix, pack_ix, unpack_inner_ix, unpack_ix, BufferWindow, BeachOps},
+    use crate::transaction::instructions::{
+        pack_inner_ix, pack_ix, unpack_inner_ix, unpack_ix, BeachOps, BufferWindow,
     };
 
     #[test]
@@ -137,8 +156,8 @@ mod tests {
 
         let inner_ixpath = "ix_beachbuffer_test12359132.beach";
 
-        let packed       = pack_ix(&serde_json::from_str(sample_ix2).unwrap());
-        let mut f        = File::create(inner_ixpath).unwrap();
+        let packed = pack_ix(&serde_json::from_str(sample_ix2).unwrap());
+        let mut f = File::create(inner_ixpath).unwrap();
         f.write_all(packed.as_slice()).unwrap();
 
         let f = File::open(inner_ixpath).unwrap();
@@ -146,11 +165,10 @@ mod tests {
         let mut buffer: Vec<u8> = Vec::new();
         reader.read_to_end(&mut buffer).unwrap();
 
-        let mut bw= BufferWindow{
+        let mut bw = BufferWindow {
             buffer: &buffer,
-            offset:Arc::new(Mutex::new(0))
+            offset: Arc::new(Mutex::new(0)),
         };
-
 
         let ix = bw.extract_instruction();
         println!("Bw offset is now {:?}", bw.offset);
@@ -221,17 +239,16 @@ mod tests {
                     }"#;
 
         let inner_ixpath = "inner_ix_test124142.beach";
-        let packed       = pack_inner_ix(&serde_json::from_str(sample_inner_ix_raw).unwrap());
-        let mut f        = File::create(inner_ixpath).unwrap();
+        let packed = pack_inner_ix(&serde_json::from_str(sample_inner_ix_raw).unwrap());
+        let mut f = File::create(inner_ixpath).unwrap();
         f.write_all(packed.as_slice()).unwrap();
 
         let f = File::open(inner_ixpath).unwrap();
         let mut reader = BufReader::new(f);
         let mut buffer: Vec<u8> = Vec::new();
         reader.read_to_end(&mut buffer).unwrap();
-
+        println!("Returning r :{:#04X?}", &buffer);
         let inner_ix = unpack_inner_ix(&buffer);
-        
 
         assert!(inner_ix.eq(&InnerInstructions {
             index: 0,
@@ -280,8 +297,8 @@ mod tests {
 
         let inner_ixpath = "ix_test1235912.beach";
 
-        let packed       = pack_ix(&serde_json::from_str(sample_ix2).unwrap());
-        let mut f        = File::create(inner_ixpath).unwrap();
+        let packed = pack_ix(&serde_json::from_str(sample_ix2).unwrap());
+        let mut f = File::create(inner_ixpath).unwrap();
         f.write_all(packed.as_slice()).unwrap();
 
         let f = File::open(inner_ixpath).unwrap();
@@ -290,7 +307,6 @@ mod tests {
         reader.read_to_end(&mut buffer).unwrap();
 
         let (ix, _) = unpack_ix(&buffer);
-        
 
         assert_eq!(ix, CompiledInstruction{
             accounts: vec![1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
@@ -363,17 +379,5 @@ mod tests {
     }
     // --------- (((((((())))))))
 
-    
-
-
-
-
-    
-
-
-
-
     // ---------------------------------------- INNER IXs
-
-    
 }
